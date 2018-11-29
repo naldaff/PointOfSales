077bf2f7c7f1af3ddb5030d4dec4d1a985db2e5d
Rizky Said <rizkysaid40@gmail.com>
2018-11-29 16:46:45 +0700

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+	public function index(){		
+	    $categories = Category::orderBy('created_at', 'ASC')->paginate(10);
+	    return view('categories.index', compact('categories'));
+	}
+
+	public function store(Request $request){
+		//validasi
+		$this->validate($request, [
+			'name' => 'required|string|max:50',
+			'description' => 'nullable|string'
+		]);
+
+		try{
+			$categories = Category::firstOrCreate([
+				'name' => $request->name
+			],[
+				'description' => $request->description
+			]);
+			return redirect()->back()->with(['success' => 'Kategori '. $categories->name . ' Berhasil ditambahkan!']);
+		}catch(\Exception $e){
+			return redirect()->back()->with(['error' => $e->getMessage() ]);
+		}
+	}
+
+	public function destroy($id){
+		$categories = Category::findOrFail($id);
+		$categories->delete();
+		return redirect()->back()->with(['success' => 'Kategori '. $categories->name . 'Dihapus!']);
+	}
+
+	public function edit($id){
+		$categories = Category::findOrFail($id);
+		return view('categories.edit', compact('categories'));
+	}
+
+	public function update(Request $request, $id){
+		//validasi form
+		$this->validate($request, [
+			'name' => 'required|max:50',
+			'description' => 'nullable|string'
+		]);
+
+		try{
+			//select data berdasarkan id
+			$categories = Category::findOrFail($id);
+
+			//update data
+			$categories->update([
+				'name' => $request->name,
+				'description' => $request->description
+			]);
+
+			//redirect ke route kategori index
+			return redirect(route('kategori.index'))->with(['success' => 'Kategori ' . $categories->name . ' Berhasil diupdate!']);
+		}catch(\Exception $e){
+			return redirect()->back()->with(['error' => $e->getMessage()]);
+		}
+	}
+}
---
+++ resources/views/layouts/module/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2018-2019 <a href="http://adminlte.io">ISKY POS</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.1.
+    </div>
+</footer>
